e44c2003ad9726e8f761d20f326be8a55c0a8bb5
Emanuel Lauria <emanuel.lauria@gmail.com>
2013-07-02 14:50:28 +0200

    fullscreen mode opens in new tab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $('#example').css('height', $(example).width() * 9/16 -100)
+
     $('#fullscreen').click(function() {
 
         window.open($('iframe').attr('src'), '_blank');
