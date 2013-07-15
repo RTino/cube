72d93adb6ec8a17f59ec7caaad5aebeffd6f730c
Igor Ramadas <igor@devv.com>
2013-07-15 11:21:51 +0200

    New index.js file to start the cube. Updated description on install.sh.

---
+++ index.js
@@ -0,0 +1,6 @@
+// CUBE INDEX START
+// -------------------------------------------------------------------------
+// This is a shim file to run the `cube.coffee`.
+
+require("coffee-script");
+require("./cube.coffee");
